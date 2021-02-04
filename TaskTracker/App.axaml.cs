5ddf764752b076e3f079ca11e5895b128b4aa6d5
Marcel Walk <walk.marcel.97@googlemail.com>
2021-02-04 22:17:11 +0100

    Add project files.

---
+++ TaskTracker/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using TaskTracker.ViewModels;
+using TaskTracker.Views;
+
+namespace TaskTracker
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
